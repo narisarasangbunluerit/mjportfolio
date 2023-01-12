3ebe91b651fbc6bb77c0b31c1a94f3b00460fee0
mj <narisara.jun43@gmail.com>
2023-01-13 01:46:41 +0700

    Updates

---
+++ precache-manifest.5bc1e4214972dd3ae6424a64dbe7c4b4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6a1e4f0111eb4cc1810",
+    "url": "/mjportfolio/static/css/main.890a14b8.chunk.css"
+  },
+  {
+    "revision": "d6a1e4f0111eb4cc1810",
+    "url": "/mjportfolio/static/js/main.e2c5c229.chunk.js"
+  },
+  {
+    "revision": "67459c6ff6c70a9eded2",
+    "url": "/mjportfolio/static/js/runtime~main.6baa6216.js"
+  },
+  {
+    "revision": "61f015b16e5935703bf4",
+    "url": "/mjportfolio/static/css/2.b323f339.chunk.css"
+  },
+  {
+    "revision": "61f015b16e5935703bf4",
+    "url": "/mjportfolio/static/js/2.d7e506d5.chunk.js"
+  },
+  {
+    "revision": "3faa7b265314843a148a",
+    "url": "/mjportfolio/static/js/3.cfed6fcf.chunk.js"
+  },
+  {
+    "revision": "7dbb5e670523330178ebff420715e037",
+    "url": "/mjportfolio/static/media/App1.7dbb5e67.png"
+  },
+  {
+    "revision": "3d62a4b12b3ec8f0b2ea89104e4c0ffb",
+    "url": "/mjportfolio/static/media/App2.3d62a4b1.png"
+  },
+  {
+    "revision": "ea4729cfe08889b5afe26cbb19fae754",
+    "url": "/mjportfolio/static/media/Web1.ea4729cf.png"
+  },
+  {
+    "revision": "acebffa8153585b8fce3bd6b0bc41fe1",
+    "url": "/mjportfolio/static/media/Web2.acebffa8.png"
+  },
+  {
+    "revision": "76ff08b9e3187c05202b393420fc6eb3",
+    "url": "/mjportfolio/static/media/Web3.76ff08b9.png"
+  },
+  {
+    "revision": "6a3ceca809eacaa1e34df49c2c236c81",
+    "url": "/mjportfolio/static/media/Web4.6a3ceca8.png"
+  },
+  {
+    "revision": "854cc33d2775d30659b85a2b33076ecf",
+    "url": "/mjportfolio/static/media/Web5.854cc33d.png"
+  },
+  {
+    "revision": "ab290a5fdeca7188e7676f90884c5f94",
+    "url": "/mjportfolio/static/media/Web6.ab290a5f.png"
+  },
+  {
+    "revision": "985b5d55d20271863eadae2c63bd9089",
+    "url": "/mjportfolio/static/media/logo.985b5d55.png"
+  },
+  {
+    "revision": "e190e207d2832a6c0f675064980fce03",
+    "url": "/mjportfolio/static/media/about.e190e207.jpg"
+  },
+  {
+    "revision": "8ddebba1598395f35eaadcba1dfadafe",
+    "url": "/mjportfolio/static/media/John-Cv.8ddebba1.pdf"
+  },
+  {
+    "revision": "d3ae908b297f9940dceb28931bfb0102",
+    "url": "/mjportfolio/static/media/testimonial1.d3ae908b.png"
+  },
+  {
+    "revision": "d3b5c0f116d0c7f40a33a249170b5173",
+    "url": "/mjportfolio/static/media/testimonial2.d3b5c0f1.png"
+  },
+  {
+    "revision": "27410f28283ec764c121c0e0f5d1f7bb",
+    "url": "/mjportfolio/static/media/testimonial3.27410f28.png"
+  },
+  {
+    "revision": "85b252bcba983c0131ca4c3f1d6eca6c",
+    "url": "/mjportfolio/static/media/testimonial4.85b252bc.png"
+  },
+  {
+    "revision": "6ae17b75906e01fda735244e61f73477",
+    "url": "/mjportfolio/static/media/testimonial5.6ae17b75.png"
+  },
+  {
+    "revision": "ca9be7289e8c27c83610966c4067be8b",
+    "url": "/mjportfolio/static/media/testimonial6.ca9be728.png"
+  },
+  {
+    "revision": "97b9b4e3262202fdf0ff622aee0b47a5",
+    "url": "/mjportfolio/static/media/footer.97b9b4e3.png"
+  },
+  {
+    "revision": "d275020632a92bad852480e26e6409cc",
+    "url": "/mjportfolio/static/media/profile.d2750206.jpg"
+  },
+  {
+    "revision": "1bef2f3627b0b51086e988b8d0e9f06f",
+    "url": "/mjportfolio/index.html"
+  }
+];
